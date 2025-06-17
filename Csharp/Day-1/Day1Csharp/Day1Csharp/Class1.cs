9f2b33f68b284ff044a3823848d5f941c7e84f81
Dinesh Kapu <dineshkap@infinite.com>
2025-06-17 22:35:53 +0530

    program

---
+++ Csharp/Day-1/Day1Csharp/Day1Csharp/Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Csharp
+{
+    class ControlStatements
+    {
+        #region
+        static void Main()
+        {
+            ControlStatements cs = new ControlStatements();
+            //char retgrade=cs.CheckGrade();
+            //Console.WriteLine("The Grade is:{0}",retgrade);
+            //OR
+            //Console.WriteLine(cs.CheckGrade());
+            //cs.CheckGrade_with_Switch();
+            IterationStatements loops = new IterationStatements();
+            loops.WhileLoop();
+            Console.WriteLine("*****");
+            loops.DoWhileLoop();
+            Console.WriteLine("*****");
+            loops.ForLoop();
+            Console.WriteLine("*****");
+      
+            loops.ForEachLoop();
+            Console.Read();
+        }
+        #endregion
+        #region
+        public char CheckGrade() 
+        { 
+          char grade;
+          Console.WriteLine("Enter your Grade:");
+           grade = Convert.ToChar(Console.ReadLine());
+           if(grade=='O' || grade == 'o')
+            {
+                Console.WriteLine("Outstanding");
+            }
+           else if(grade=='A' || grade == 'a')
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (grade == 'B' || grade == 'b')
+            {
+                Console.WriteLine("Very Good");
+            }
+            else if (grade == 'C' || grade == 'c')
+            {
+                Console.WriteLine("Fair");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            return grade;
+        }
+        #endregion
+        /// <summary>
+        ///     The below Function works on multiple switch Claues
+        /// </summary>
+        public void CheckGrade_with_Switch()
+        {
+            char grade;
+            Console.WriteLine("Enter the Grade:");
+            grade = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch(grade)
+            {
+
+                case 'O':
+                    Console.WriteLine("Outstanding");
+                    break;
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Vey Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Fair");
+                    break;
+                default:
+                    Console.WriteLine("Fail");
+                    break;
+            }
+        }
+    }
+   
+    class IterationStatements
+    {
+        public void WhileLoop()
+        {
+            int i = 1;
+            while(i<=5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        public void DoWhileLoop()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <=5);
+        }
+
+        public void ForLoop()
+        {
+            int tot = 0;
+            int i = 0;
+            for(;i<=5;i++)
+            {
+                tot += i;
+            }
+            Console.WriteLine("Sum of all numbers:{0}", tot);
+        }
+        public void ForEachLoop()
+        {
+            int[] data = new int[] { 76, 4, 12, 0, 3 };
+            Console.WriteLine(data.Length);
+            Console.WriteLine("--Before Sort---");
+            foreach(int x in data)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("---After Sort----");
+            Array.Sort(data);
+            foreach (int x in data)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
---
+++ Csharp/Day-1/Day1Csharp/Day1Csharp/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConversionFunction();
+            //Program prog = new Program();
+            //prog.SampleTernary();
+            //Console.WriteLine("******");
+            //Program.ConversionFunction();
+            //TestTryParse.tryParse_Function();
+            //TestTryParse.Implicit_Types();
+            TestTryParse.NullableEg();
+            Console.Read();
+        }
+        public static void ConversionFunction() //static member or class member
+        {
+            Console.WriteLine("Min int value is {0}", int.MinValue);
+            Console.WriteLine("Max int value is {0}", int.MaxValue);
+            int i = 100;
+            float f = i; //implict Conversion
+            float f2 = 123.75f;
+            i = (int)f2;//explict Conversion by typecasting
+            Console.WriteLine(i);
+            i = Convert.ToInt32(f2);//explict conversion by using Conversion Function
+            Console.WriteLine(i);
+            Console.WriteLine("Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Marks :");
+            //float marks = float.Parse(Console.ReadLine());
+            float marks = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Num ={0} and Marks={1}",num,marks);
+        }
+        public void SampleTernary()//non-static menber or instance number
+        {
+            int num = 10;
+            bool b;
+            if (num == 10)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            Console.WriteLine("Number ==10 is: {0}", b);
+            //using ternary operator
+            b = num ==10? true : false;
+            Console.WriteLine("Number ==10 is: {0}",b);
+        }
+  
+    }
+    class TestTryParse
+    {
+        public static void tryParse_Function()
+        {
+            float data = 343456782564.77f;
+            int x = (int)data; //int declared its capacity but no exception
+            //int x = Convert.ToInt32(data); //exception was raised
+           
+            Console.WriteLine(x);
+
+            //let us try with tryparse()
+            string str = "100"; //string str="100gms"
+            //x = int.Parse(str); //throwing an exception
+            int result;
+            bool status;
+            status = int.TryParse(str,out result);
+            if(status) //if true
+            {
+                Console.WriteLine($"Status is {status} and result is {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Data");
+            }
+            Console.WriteLine(x);
+        }
+        public static  void Box_unbox()
+        {
+            int i = 10;
+            object obj = i; //boxing
+
+            i = (int)obj; //unboxing
+
+            float percent = Convert.ToSingle(Console.ReadLine());
+
+            string s = "AAA";
+            obj = s;//implict
+            s =(string)obj;//explict typecast
+            s = Convert.ToString(obj);//explicit conversion
+
+        }
+        public static void Implicit_Types()
+        {
+            int x;
+            x = 8;
+            var v=3.4f;
+            Console.WriteLine(v);
+            dynamic d;
+            d = 5;
+            d = "Aa";
+            d = false;
+            Console.WriteLine(d);
+        
+        }
+        public static void NullableEg()
+        {
+            string s = null;
+            int ? number = 100;//enabling a value type to also have null values
+            int total;
+            if(number==null)
+            {
+                total = 0;
+            }
+            else
+            {
+                //total = (int)number;//or
+                total = number.Value;
+            }
+            //the above decision clause can be replaced with coalescing operator
+            total = number ?? 0;//if number is null ,then it will display 0
+            Console.WriteLine(total);
+            float ? f = null;
+            int ? ticketsonsale = Convert.ToInt32(Console.ReadLine());
+            int availabletickets;
+            if(ticketsonsale==null)
+            {
+                availabletickets = 0;
+            }
+            else
+            {
+                //availabletickets = (int)ticketsonsale;
+                availabletickets = ticketsonsale.Value;
+            }
+            Console.WriteLine("the available tickets:{0}",availabletickets);
+          
+        }
+    }
+}
